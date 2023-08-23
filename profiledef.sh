6c69021a7d7207fb4459b2a1aac761457645cc21
Florian <florian.zweiler@outlook.at>
2023-08-23 23:43:55 +0200

    Adjust profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_name="Arch_Installers_ISO"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="zweiler2 <https://github.com/zweiler2>"
+iso_application="Arch_Installers_ISO"
+iso_version="$(date +%Y%m%d_%H%M)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
